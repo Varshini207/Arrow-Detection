7ad55abe09befc1383250ea8074d02a589ef0e52
Varshini207 <168453378+Varshini207@users.noreply.github.com>
2025-09-16 05:39:08 +0530

    Testing File 1 for Arrow Detection

---
+++ test1.py
@@ -0,0 +1,44 @@
+import cv2
+ 
+    
+# test image
+q=input("enter path of testing image")
+image = cv2.imread(r"C:\Users\Varsh\Desktop\testleftarrow.jpg")
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+histogram = cv2.calcHist([gray_image], [0], 
+                         None, [256], [0, 256])
+ 
+# data1 image
+image = cv2.imread(r"C:\Users\Varsh\Desktop\right arrow.jpg")
+gray_image1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+histogram1 = cv2.calcHist([gray_image1], [0], 
+                          None, [256], [0, 256])
+ 
+# data2 image
+image = cv2.imread(r"C:\Users\Varsh\Desktop\left arrow.jpg")
+gray_image2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+histogram2 = cv2.calcHist([gray_image2], [0], 
+                          None, [256], [0, 256])
+ 
+ 
+c1, c2 = 0, 0
+ 
+# Euclidean Distance between data1 and test
+i = 0
+while i<len(histogram) and i<len(histogram1):
+    c1+=(histogram[i]-histogram1[i])**2
+    i+= 1
+c1 = c1**(1 / 2)
+ 
+
+# Euclidean Distance between data2 and test
+i = 0
+while i<len(histogram) and i<len(histogram2):
+    c2+=(histogram[i]-histogram2[i])**2
+    i+= 1
+c2 = c2**(1 / 2)
+ 
+if(c1<c2):
+    print("The given image is a right arrow")
+else:
+    print("The given image is a left arrow")
